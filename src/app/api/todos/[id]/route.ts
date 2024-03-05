77353e782821e26e4e1c5aa8be21d2d6ad8d0007
Alonso-FernandezV <awesomenamebro@hotmail.com>
2024-03-04 22:52:21 -0700

    get todos from rest api + styles del infierno

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,7 @@
 import prisma from '@/lib/prisma'
+import { Todo } from '@prisma/client';
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup'
 
 interface Segments {
     params: {
@@ -7,17 +9,52 @@ interface Segments {
     }
 }
 
+
+const getTodo = async (id:string):Promise<Todo | null> =>{
+    
+
+    const todo = await prisma.todo.findFirst({where: { id: id }})
+
+    return todo;
+}
+
 export async function GET(request: Request, {params}: Segments) { 
 
-    const {id} = params
     
-    const todo = await prisma.todo.findFirst({
-        where:{id: id}
-    })
+    const todo = await getTodo(params.id)
 
-    if (!todo) return NextResponse.json({
-        message: `Todo with id ${id} not found`
-    }, { status: 404 })
+    if (!todo) return NextResponse.json({message: `Todo with id ${params.id} not found`}, { status: 404 });
 
   return NextResponse.json({todo:todo, hola:'Kami-sama'})
+}
+
+
+const putSchema = yup.object({
+    description: yup.string().optional(),
+    complete: yup.boolean().optional(),
+})
+
+export async function PUT(request: Request, { params }: Segments) {
+
+
+    const todo = await getTodo(params.id);
+
+    if (!todo) return NextResponse.json({
+        message: `Todo with id ${params.id} not found`
+    }, { status: 404 });
+
+    try {
+
+    } catch(error){
+        return NextResponse.json(error,{status:400});
+    }
+
+    const { complete, description} = await putSchema.validate(await request.json());
+
+    const updatedTodo = await prisma.todo.update({
+        where: {id: params.id},
+        data: {complete, description}
+    })
+
+    return NextResponse.json(updatedTodo);
 }
